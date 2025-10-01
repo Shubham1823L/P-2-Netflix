fd420a70ada5444bf9a3c6068fcda5e486e4ffd5
Shubham1823L <shubhuisbetter@gmail.com>
2025-10-02 00:42:09 +0530

    signup page 2 got further additions

--- backend.js
+++ backend.js
@@ -21,3 +21,33 @@ Array.from(document.querySelectorAll(".signupForm")).forEach(form => {
         }
     })
 })
+
+
+Array.from(document.querySelectorAll(".closeBtn")).forEach(e => {
+    e.addEventListener("click", () => {
+        let dialogBox = e.parentElement
+        dialogBox.style.opacity = 0
+        dialogBox.style.translate = "0 100px"
+        setTimeout(() => {
+            dialogBox.style.display = "none"
+        }, 500);
+
+    })
+});
+
+document.querySelector(".enterOtp").addEventListener("keydown", (e) => {
+    const btn = document.querySelector(".next")
+    setTimeout(() => {
+        if (e.target.value.length === 6) {
+            btn.style.backgroundColor = "#E50815"
+            btn.style.cursor = "pointer"
+            btn.style.opacity = 1
+        }
+        else {
+            btn.style.backgroundColor = "#D9D9D9"
+            btn.style.cursor = "not-allowed"
+            btn.style.opacity = 0.6
+        }
+
+    },1);
+})
--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ const app = express()
 const router = express.Router()
 const Joi = require("joi")
 const port = 3000
+const nodemailer = require("nodemailer")
 app.use(express.static("../"))
 app.use(express.json())
+
+
 app.get("/", (req, res) => {
     console.log("HAHAHAH")
     res.status(200).sendFile("../index.html", { root: __dirname })
 })
-app.get("/signup", (req, res) => {
-    console.log("YO")
-    res.sendFile("../signup.html", { root: __dirname })
-})
+// app.get("/signup", (req, res) => {
+//     console.log("YO")
+//     res.sendFile("../signup.html", { root: __dirname })
+// })
 app.post("/", (req, res) => {
     const schema = Joi.object({
         email: Joi.string().email().required()
@@ -28,3 +31,23 @@ app.post("/", (req, res) => {
 app.listen(port, () => {
     console.log("Listening to port:", port)
 })
+
+
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "shubhuprodev@gmail.com",
+        pass: "tldtxtqfutevumjt"
+    }
+})
+
+// async function otpSender(email){
+//     const info = await transporter.sendMail({
+//         from: `"Shubham <shubhuprodev@gmail.com>"`,
+//         to: "shubhuisbetter@gmail.com",
+//         text: "Why don't you try to walk 242125 steps today",
+//         subject: "This is a fun message from a great Dev",
+//     })
+//     console.log("Message sent:", info.messageId)
+// }
